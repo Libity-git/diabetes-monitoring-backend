fd0ba4a314f217454f597e4aa99421191ba9501b
Libity-git <thanawat.rattanakitt@gmail.com>
2025-06-13 15:22:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-// 📁 server.js
+/* // 📁 server.js
 const app = require('./src/app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+ */
+// 📁 server.js
+const app = require('./src/app');
+const PORT = process.env.PORT || 1000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
