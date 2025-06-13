29e94c195202816b477039d2e25f2862cf039b9e
Libity13 <rock80537@gmail.com>
2025-06-13 15:10:34 +0700

    อปเดต lineWebhookController.js สำหรบการสงคานำตาลและความดนพรอมกน และเพม server.js กบ backup

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 // 📁 server.js
-const app = require('./src/app');
-const PORT = process.env.PORT || 5000;
+//const app = require('./src/app');
+//const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+//app.listen(PORT, () => {
+//  console.log(`🚀 Server running on http://localhost:${PORT}`);
+//});
+
+
+// 📁 server.js
+const app = require('./src/app');
+const PORT = process.env.PORT || 1000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
